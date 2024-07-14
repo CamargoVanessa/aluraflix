3f88d8137b1fe5468c7d48a3aa7e866ab43e30b6
Vanessa Camargo <158234704+CamargoVanessa@users.noreply.github.com>
2024-07-14 00:47:39 -0300

    Add files via upload

---
+++ aluraflix/src/componentes/Rodape/index.js
@@ -0,0 +1,16 @@
+import styles from './Rodape.module.css'
+import { IoLogoInstagram, IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
+import logo from './logo.png'
+import { Link } from 'react-router-dom';
+
+function Rodape() {
+    return (
+        <footer className={styles.rodape}>
+            <Link to="./" className={styles.logo}>
+                <img src={logo} alt="LogoAluraFlix"/>
+            </Link>
+    </footer>
+    )
+}
+
+export default Rodape
